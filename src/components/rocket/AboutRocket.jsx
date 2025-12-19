f8cd04a5d70d44fba4e4cf52f410908dfd7987ef
Shang-Lun <fntxxx.gmail.com>
2025-12-19 16:59:20 +0800

    feat: add about rocket title and  description

--- src/components/rocket/AboutRocket.jsx
+++ src/components/rocket/AboutRocket.jsx
@@ -1,7 +1,25 @@
 function AboutRocket() {
     return (
         <>
-            <h1> 火箭隊介紹</h1>
+            <section className="bg-Primary-Blue-100 w-full r-lg px-12 py-9 flex flex-col items-center">
+                <div className="max-w-83 w-full flex flex-col gap-4">
+                    <div>
+                        <h2 className="body-2 font-en text-Neutral-700 font-bold">About Rocket</h2>
+                        <h2 className="h1 text-Neutral-700">什麼是火箭隊</h2>
+                    </div>
+                    <p className="text-Neutral-700">
+                        火箭隊位於高雄的工程師培訓營，針對市場急需的前端、後端工程師與 UI 設計師職缺而設。為期<span className="font-bold">七個月的免費課程</span>，學員全程參與，<span className="font-bold">僅需支付場地費</span>。
+                    </p>
+                    <p className="text-Neutral-700">
+                        火箭隊培育具備問題解決能力的專業人才，滿足企業需求。透過教練的觀察與協助，學員將建立階段性目標，確保技能成長。加入火箭隊，您將獲得實戰經驗，迅速融入職場，並滿足市場對高品質工程師的渴求。
+                    </p>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </section >
         </>
     );
 }
