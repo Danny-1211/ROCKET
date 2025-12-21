6f75cdc64aa10689d40229de617fc5af54487c50
danny <danny21316@gmail.com>
2025-12-21 14:55:09 +0800

    feat: added header component and marquee animation

--- src/pages/Rocket.jsx
+++ src/pages/Rocket.jsx
@@ -2,7 +2,6 @@ import AboutRocket  from "../components/rocket/AboutRocket.jsx";
 function Rocket() {
     return (
         <>
-            <h1>Rocket Page</h1>
             <AboutRocket />
         </>
     );
