8648db8563b25e932edc9d749cc2220cb2027ebb
ho <howardxsheng@gmail.com>
2025-12-21 14:54:33 +0800

    feat: add  workdpace areas data

---
+++ src/components/workspace/AreaContent.jsx
@@ -0,0 +1,35 @@
+import restSm from "../../assets/imgs/intro/intro-rest-sm-3x.png";
+import restLg from "../../assets/imgs/intro/intro-rest-lg-3x.png";
+import disscusSm from "../../assets/imgs/intro/intro-discuss-sm-3x.png";
+import disscusLg from "../../assets/imgs/intro/intro-discuss-lg-3x.png";
+import resiSm from "../../assets/imgs/intro/intro-worksapce-sm-3x.png";
+import resiLg from "../../assets/imgs/intro/intro-workspace-lg-3x.png";
+const areas = [
+  {
+    id:"rest",
+    title: "休憩區",
+    subtitle: "\ 很 chill 的休憩區 /",
+    desc: "我們貼心提供 飲水機、冰箱、微波爐、果汁機、膠囊咖啡機 及不定時茶包及小餅乾零食，我們想讓在工作認真打拼之餘的你也是需要中場休息補充能量，然後再輕鬆泡上一杯咖啡坐在吧台或懶骨頭放空冥想，讓自己 chill 一下轉換心情！",
+    imgSm: restSm,
+    imgLg: restLg,
+    tag: "#硬體",
+  },
+  {
+    id:"discussion",
+    title: "discussion",
+    subtitle: "\ 舒適沙發討論區 /",
+    desc: "如果你有想要與夥伴們討論事情這裡也很適合三、五人一起共同開會討論哦！",
+    imgSm: disscusSm,
+    imgLg: disscusLg,
+    tag: "#硬體",
+  },
+  {
+  id:"resident",
+  title: "Resident",
+  subtitle: "\ 進駐者區域 / ",
+  desc: "我們的目標是凝聚更多開發者，為他們提供一個充滿活力的交流空間。在這裡，開發者可以互相學習、分享，透過交流讓自己的技能和視野得到更快速的成長。我們相信，一個好的環境能夠激發創意，促進專業能力的提升。",
+  imgSm: resiSm,
+  imgLg: resiLg,
+  tag: "#硬體",
+  },
+];
