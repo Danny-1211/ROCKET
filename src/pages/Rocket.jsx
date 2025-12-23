714ad01a47391a99182e7019906cd1a6874e59fc
Shang-Lun <fntxxx.gmail.com>
2025-12-23 09:12:55 +0800

    feat: complete AboutTraining section layout and content

--- src/pages/Rocket.jsx
+++ src/pages/Rocket.jsx
@@ -1,10 +1,12 @@
 import AboutRocket from "../components/rocket/AboutRocket.jsx";
 import SuccessCases from "../components/rocket/SuccessCases.jsx";
+import AboutTrining from "../components/rocket/AboutTraining.jsx";
 function Rocket() {
     return (
         <>
             <AboutRocket />
             <SuccessCases />
+            <AboutTrining />
         </>
     );
 }
