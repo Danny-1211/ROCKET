00954638723ddf00c6317262184e0585ac80e740
Shang-Lun <fntxxx.gmail.com>
2025-12-24 14:42:13 +0800

    refactor: about-training structure

--- src/data/rocket/trainingData.jsx
+++ src/data/rocket/trainingData.jsx
@@ -5,7 +5,27 @@ import backendSm from '../../assets/imgs/about-training/backend-sm.png';
 import backendLg from '../../assets/imgs/about-training/backend-lg.png';
 import UISm from '../../assets/imgs/about-training/UI-sm.png';
 import UILg from '../../assets/imgs/about-training/UI-lg.png';
-import Highlight from './Highlight';
+
+function Highlight({ children }) {
+    return (
+        <span
+            className="
+                font-bold
+                relative z-0 inline-block
+                after:content-['']
+                after:absolute
+                after:left-0
+                after:top-2
+                after:w-full
+                after:h-3
+                after:bg-accent-yellow
+                after:-z-10
+            "
+        >
+            {children}
+        </span>
+    );
+}
 
 const trainingData = [
     {
