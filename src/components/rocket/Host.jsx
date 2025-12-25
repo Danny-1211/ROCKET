574523881b8f45ecb47fa2a3aef62797446e4272
Shang-Lun <fntxxx.gmail.com>
2025-12-25 14:45:24 +0800

    style: adjust Host section layout

--- src/components/rocket/Host.jsx
+++ src/components/rocket/Host.jsx
@@ -63,44 +63,43 @@ function Host() {
   const drag = useDragScroll();
 
   return (
-    <div className="relative mt-31">
-      <p className="h3 absolute top-2  left-18  -translate-y-1/2">
-        主辦、協辦單位
-      </p>
-      <div className="w-full  bg-Neutral-200  overflow-hidden  flex flex-col items-start justify-start flex-nowrap pt-9 pb-4 px-7 md:pt-24 lg:pt-24 lg:pb-10 lg:px-18">
+    <section className="bg-Neutral-200 relative mt-19 md:mt-31 ">
+      <div className="w-full  flex flex-col items-start justify-start flex-nowrap pt-9 pb-4 pl-12 md:pl-3 md:pt-14 md:mx-auto md:max-w-324">
+        <p className="h3 absolute top-2 -translate-y-1/2">
+          主辦、協辦單位
+        </p>
         <div
           ref={drag.ref}
           {...drag.handlers}
           className="
-            w-full
+            w-full pr-12
             overflow-x-auto
             cursor-grab active:cursor-grabbing
             select-none
-            [&::-webkit-scrollbar]:hidden
-            [-ms-overflow-style:none]
-            [scrollbar-width:none]
+            scrollbar-hide
+            md:pr-3
           "
         >
-          <ul className="flex gap-5 px-12 lg:px-0 lg:gap-6 w-max">
+          <ul className="flex gap-5 md:gap-6 w-max">
             {compony.map((item, index) => {
               return (
                 <li
                   className=" flex flex-col items-start justify-start gap-4 shrink-0"
                   key={index}
                 >
-                  <div className="  r-sm bg-Neutral-white flex items-center justify-center w-39 h-30 lg:w-49 lg:h-37 lg:p-4">
+                  <div className="  r-sm bg-Neutral-white flex items-center justify-center w-39 h-30 md:w-49 md:h-37 md:p-4">
                     <img
-                      className="hidden lg:block w-full h-full object-contain  "
+                      className="hidden md:block w-full h-full object-contain  "
                       src={item.imgUrl_lg}
                       alt={item.name}
                     />
                     <img
-                      className="block w-25 h-full object-contain lg:hidden "
+                      className="block w-25 h-full object-contain md:hidden "
                       src={item.imgUrl_sm}
                       alt={item.name}
                     />
                   </div>
-                  <div className="flex flex-col justify-start items-start lg:gap-1">
+                  <div className="flex flex-col justify-start items-start md:gap-1">
                     <p className="font-bold">{item.unit}</p>
                     <p>{item.name}</p>
                   </div>
@@ -110,7 +109,7 @@ function Host() {
           </ul>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
