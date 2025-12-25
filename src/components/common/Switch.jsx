8e6a00e3cafdcb39b6ccfe704d76547ff54c2eb2
Danny-1211 <danny21316@gmail.com>
2025-12-25 21:31:03 +0800

    style: adjust banner layout and animation

--- src/components/common/Switch.jsx
+++ src/components/common/Switch.jsx
@@ -11,18 +11,24 @@ import rocket_card_highlight_lg from "../../assets/imgs/icon/ic-highlight-cowork
 import coworkin_lg from "../../assets/imgs/icon/banner-logo-coworking-lg.svg";
 import coworkin_card_btn_lg from "../../assets/imgs/icon/banner-btn-coworking-lg.svg";
 import coworkin_card_highlight_lg from "../../assets/imgs/icon/ic-highlight-rocket-lg.svg";
-
+import bannerBgImg from "../../assets/imgs/bg/banner-bg.png";
 function Switch() {
   const banner_bg = `bg-[url('../assets/imgs/bg/banner-bg.png')]`;
   const isRocket = 1;
   const isWorkspace = 2;
   const [isSelected, setIsSelected] = useState(isRocket);
+
+  // 未選中時淡化 60% ，Hover時恢復 100%
+  const unselectedStyle = "opacity-60 group-hover:opacity-100 transition-opacity duration-300";
+
   return (
     <>
+      {/* Mobile Version  */}
       <section
-        className={` w-full ${banner_bg} bg-repeat py-30 px-10  md:hidden lg:hidden`}
+        className={`w-full  py-30 px-10 md:hidden lg:hidden`}
+        style={{ backgroundImage: `url(${bannerBgImg})` }}
       >
-        <div className="btn-group-mobile flex flex-col items-center justify-center gap-11.5  ">
+        <div className="btn-group-mobile flex flex-col items-center justify-center gap-11.5">
           <div
             className={`rocket-btn-mobile relative bg-Neutral-white border-2 border-Neutral-300 r-md px-6 py-6 w-73.5 h-33.5 
             ${
@@ -100,11 +106,17 @@ function Switch() {
         </div>
       </section>
 
+      {/* Desktop Version  */}
       <section
-        className={`hidden w-full ${banner_bg} bg-repeat md:flex md:items-center md:justify-center lg:flex lg:items-center lg:justify-center `}
+        className={`hidden w-full bg-repeat md:flex md:items-center md:justify-center  lg:flex lg:items-center lg:justify-center overflow-hidden`}
+        style={{ backgroundImage: `url(${bannerBgImg})` }}
       >
-        <div className="btn-group-desktop flex items-center justify-between py-30.5 px-45.5 ">
-          <div className={`group-rocket-desktop flex flex-col cursor-pointer transform-all duration-300 ${isSelected !== isRocket ? "hover:-mt-4" : ""}`}>
+        <div className="btn-group-desktop flex items-center justify-center w-full gap-10 py-30.5 transition-transform duration-300 origin-center md:scale-[0.65] lg:scale-100 xl:scale-100">
+          <div
+            className={`group-rocket-desktop group flex flex-col cursor-pointer transform-all duration-300 ${
+              isSelected !== isRocket ? "hover:-translate-y-3" : ""
+            }`}
+          >
             <div
               className={`rocket-btn-desktop relative bg-Neutral-white border-2 border-Neutral-300 r-md flex flex-col justify-center items-center w-110 h-112
                     ${
@@ -116,8 +128,9 @@ function Switch() {
               onClick={() => setIsSelected(isRocket)}
             >
               {isSelected !== isRocket && (
-                <div className="absolute inset-0 bg-white/60 z-20" />
+                <div className="absolute inset-0 bg-Neutral-white/60 z-20 transition-opacity duration-300 group-hover:opacity-0 pointer-events-none" />
               )}
+
               <img
                 src={rocket_card_highlight_lg}
                 alt="highlight"
@@ -125,45 +138,65 @@ function Switch() {
                   isSelected === isRocket ? "block" : "hidden"
                 }`}
               />
+
               <div
                 className={`rocket-title-desktop flex flex-col items-center justify-center -mt-33 z-30
-                ${!(isSelected === isRocket) ? "opacity-60" : ""}`}
+                ${!(isSelected === isRocket) ? unselectedStyle : ""}`}
               >
                 <p className="h1">火箭隊</p>
                 <p className=" h4 text-Neutral-500 ">Bootcamp Rocket</p>
               </div>
-              <div className="rocket-icon-desktop pt-6 pb-4">
+
+              <div
+                className={`rocket-icon-desktop pt-6 pb-4 ${
+                  !(isSelected === isRocket) ? unselectedStyle : ""
+                }`}
+              >
                 <img className="object-contain" src={rocket_lg} alt="rocket" />
               </div>
-              <div className="rocket-tag-desktop flex flex-col justify-center items-center">
+
+              <div
+                className={`rocket-tag-desktop flex flex-col justify-center items-center ${
+                  !(isSelected === isRocket) ? unselectedStyle : ""
+                }`}
+              >
                 <p className="body-2 font-bold"># 軟體工程師培訓營</p>
                 <p className="body-2 font-bold"># 全程免費的扎實訓練</p>
                 <p className="body-2 font-bold"># 帶你翻轉人生！</p>
               </div>
             </div>
+
             <div className="w-30 h-35.75 relative flex items-center justify-center shrink-0 -mt-21 ml-40">
               <div
                 className={`absolute shrink-0 w-30 h-15 bg-Primary-Blue-200 rounded-b-full bottom-0`}
               >
                 {!(isSelected === isRocket) && (
-                  <div className="absolute inset-0 bg-white/60 z-10 pointer-events-none" />
+                  <div className="absolute inset-0 bg-white/60 z-10 pointer-events-none transition-opacity duration-300 group-hover:opacity-0" />
                 )}
               </div>
               <img
                 src={rocket_card_btn_lg}
                 alt="rocket"
                 className={`w-25 h-27 object-cover z-30 ${
-                  !(isSelected === isRocket) ? "opacity-60" : ""
+                  !(isSelected === isRocket)
+                    ? "opacity-60 group-hover:opacity-100 transition-opacity duration-300"
+                    : ""
                 }`}
               />
             </div>
           </div>
-          <div className="btn-and py-8.5 px-20.5">
+
+          <div className="btn-and px-4">
             <p className="h1">X</p>
           </div>
-          <div className={`group-workspace-desktop flex flex-col cursor-pointer transform-all duration-300 ${isSelected !== isWorkspace ? "hover:-mt-4" : ""}`}>
+
+          <div
+            className={`group-workspace-desktop group flex flex-col cursor-pointer transform-all duration-300 ${
+              isSelected !== isWorkspace ? "hover:-translate-y-3" : ""
+            }`}
+          >
             <div
-              className={`workspace-btn-desktop  relative bg-Neutral-white border-2 border-Neutral-300 r-md flex flex-col justify-center items-center w-110 h-112
+              className={`workspace-btn-desktop relative bg-Neutral-white border-2 border-Neutral-300 r-md flex flex-col justify-center items-center w-110 h-112
                     ${
                       isSelected === isWorkspace
                         ? "shadow-[8px_8px_#EEE5FF]"
@@ -173,8 +206,9 @@ function Switch() {
               onClick={() => setIsSelected(isWorkspace)}
             >
               {isSelected !== isWorkspace && (
-                <div className="absolute inset-0 bg-Neutral-white/60 z-20" />
+                <div className="absolute inset-0 bg-Neutral-white/60 z-20 transition-opacity duration-300 group-hover:opacity-0 pointer-events-none" />
               )}
+
               <img
                 src={coworkin_card_highlight_lg}
                 alt="highlight"
@@ -182,39 +216,53 @@ function Switch() {
                   isSelected === isWorkspace ? "block" : "hidden"
                 }`}
               />
+
               <div
-                className={`workspace-title-desktop flex flex-col items-center justify-center -mt-33 z-30 ${
-                  !(isSelected === isWorkspace) ? "opacity-60" : ""
-                }`}
+                className={`workspace-title-desktop flex flex-col items-center justify-center -mt-33 z-30 
+                ${!(isSelected === isWorkspace) ? unselectedStyle : ""} `}
               >
                 <p className="h1">共同空間</p>
                 <p className=" h4 text-Neutral-500 ">Co-working Space</p>
               </div>
-              <div className="workspace-icon-desktop pt-6 pb-4">
+              <div
+                className={`workspace-icon-desktop pt-6 pb-4 ${
+                  !(isSelected === isWorkspace) ? unselectedStyle : ""
+                }`}
+              >
                 <img
                   className="object-contain"
                   src={coworkin_lg}
                   alt="workspace"
                 />
               </div>
-              <div className="workspace-tag-desktop flex flex-col justify-center items-center">
+
+              <div
+                className={`workspace-tag-desktop flex flex-col justify-center items-center ${
+                  !(isSelected === isWorkspace) ? unselectedStyle : ""
+                }`}
+              >
                 <p className="body-2 font-bold"># 超 chill 的共同工作場域</p>
                 <p className="body-2 font-bold"># 餅乾、零食、咖啡應有盡有</p>
                 <p className="body-2 font-bold"># 等你來一同交流成長</p>
               </div>
             </div>
+
             <div className="w-30 h-35.75 relative flex items-center justify-center shrink-0 -mt-21 ml-40">
               <div
                 className={`absolute shrink-0 w-30 h-15 bg-Primary-Violet-200 rounded-b-full bottom-0`}
               >
                 {!(isSelected === isWorkspace) && (
-                  <div className="absolute inset-0 bg-white/60 z-10 pointer-events-none" />
+                  <div className="absolute inset-0 bg-white/60 z-10 pointer-events-none transition-opacity duration-300 group-hover:opacity-0" />
                 )}
               </div>
               <img
                 src={coworkin_card_btn_lg}
                 alt="workspace"
-                className={`w-25 h-27 object-cover z-30`}
+                className={`w-25 h-27 object-cover z-30 ${
+                  !(isSelected === isWorkspace)
+                    ? "opacity-60 group-hover:opacity-100 transition-opacity duration-300"
+                    : ""
+                }`}
               />
             </div>
           </div>
