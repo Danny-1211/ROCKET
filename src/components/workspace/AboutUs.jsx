00f11fac4042bf337c471f723228cc40cae3ae98
ho <howardxsheng@gmail.com>
2025-12-26 15:14:06 +0800

    style: adjust font bold

--- src/components/workspace/AboutUs.jsx
+++ src/components/workspace/AboutUs.jsx
@@ -8,7 +8,7 @@ function AboutUs() {
                         <p className="md:text-[20px] font-bold md:ml-1">About Us</p>
                         <h2 className="h1 mb-4 md:mb-10">共同空間簡介</h2>
                         <p className="mb-5 md:text-[20px] md:max-w-125 ">共同空間有近 100 坪的空間，在開放式的公共區域中有
-                            <span className="emphasis">休憩區以及舒適沙發討論區</span>，在設計上盡量讓空間規劃簡單俐落，沒有多餘的裝修，我們營運空間的理念很簡單，希望藉由這裡能與更多「人」締結關係，並凝聚更多熱愛分享程式的進駐培訓者，讓這裡打造實現開發能量的集聚地。</p>
+                            <span className="font-bold">休憩區以及舒適沙發討論區</span>，在設計上盡量讓空間規劃簡單俐落，沒有多餘的裝修，我們營運空間的理念很簡單，希望藉由這裡能與更多「人」締結關係，並凝聚更多熱愛分享程式的進駐培訓者，讓這裡打造實現開發能量的集聚地。</p>
                     </div>
                     <div className="aspect-square border-white r-sm border-12 md:flex-1 md:aspect-636/420">
                         <iframe
