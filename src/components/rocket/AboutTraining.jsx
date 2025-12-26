56ecde53f5bd8a6a5a57bd5ba3a21a97446c65d3
Shang-Lun <fntxxx.gmail.com>
2025-12-26 11:26:43 +0800

    refactor: replace font-bold with emphasis utility

--- src/components/rocket/AboutTraining.jsx
+++ src/components/rocket/AboutTraining.jsx
@@ -85,7 +85,7 @@ function TrainingIntro() {
 
             <div className="max-w-83 w-full mt-7.5 mx-auto px-5 py-3 text-[14px] whitespace-nowrap bg-white shadow-[0_2px_4px_0_#E8E8E8] rounded-[50px] md:text-[16px] md:max-w-159 md:mt-9.5">
                 <div className="flex gap-3 overflow-x-hidden">
-                    <div className="font-bold md:my-1.25">近期梯次</div>
+                    <div className="emphasis md:my-1.25">近期梯次</div>
                     <div className="border-2 border-Neutral-200"></div>
                     <div className="w-full md:my-1.25">
                         <TrainingBatch />
