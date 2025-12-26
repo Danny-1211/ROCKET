2b55f404f3a367614c9f2f56e21b185fcd6344c2
danny <danny21316@gmail.com>
2025-12-26 12:09:42 +0800

    update: change icon when page change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,20 +3,25 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import Rocket from "./Rocket.jsx";
 import Workspace from "./Workspace.jsx";
 import Switch from "../components/common/Switch.jsx";
-
+import rocketIcon from "../assets/imgs/icon/favicon.ico"; 
+import workspaceIcon from "../assets/imgs/icon/favicon-hexschool-logo.ico";
 function Home() {
   const location = useLocation();
 
   useEffect(() => {
+    let link = document.querySelector("link[rel~='icon']");
     switch (location.pathname) {
       case "/rocket":
         document.title = "Bootcamp Rocket";
+        link.href = rocketIcon;
         break;
       case "/workspace":
         document.title = "Co-working Space";
+        link.href = workspaceIcon;
         break;
       default:
-        document.title = "火箭隊培訓營";
+        document.title = "Bootcamp Rocket";
+        link.href = rocketIcon;
     }
   }, [location]);
 
